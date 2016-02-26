cfc85f022d60815fcc579ea3084f15795099a473
Axel Goetz <axel.goetz@hotmail.com>
2016-02-26 13:50:21 +0000

    Main uses algorithm 2

--- checking/src/main.js
+++ checking/src/main.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import d3 from 'd3';
-import alg from './algorithm1.js';
+import alg from './algorithm2.js';
 import parser from './parse.js';
 import polygonBoolean from '2d-polygon-boolean';
 
@@ -8,19 +8,27 @@ import polygonBoolean from '2d-polygon-boolean';
 let data = {"map":[[-2,-2],[-0.5,0],[5,0],[2,0.5],[0,5],[0.2,3]],"guards":[[5,0],[0,5],[-2,-2]]};
 
 function main() {
-  parser.getCheck().then(function(data) {
-  //   debugger;
-    for(var key in data) {
-      data[key].map = _.map(data[key].map, a => ({x: a[0], y: a[1]}));
-      data[key].guards = _.map(data[key].guards, a => ({x: a[0], y: a[1]}));
-      let result = alg(data[key].map, data[key].guards);
-      if(result !== null) {
-        console.log(key + ': (' + result.x + ', ' + result.y + ')');
-      } else {
-        console.log(key + ": The entire area is covered");
-      }
-    }
-  });
+  // parser.getCheck().then(function(data) {
+  // //   debugger;
+  //   for(var key in data) {
+  //     data[key].map = _.map(data[key].map, a => ({x: a[0], y: a[1]}));
+  //     data[key].guards = _.map(data[key].guards, a => ({x: a[0], y: a[1]}));
+  //     let result = alg(data[key].map, data[key].guards);
+  //     if(result !== null) {
+  //       console.log(key + ': (' + result.x + ', ' + result.y + ')');
+  //     } else {
+  //       console.log(key + ": The entire area is covered");
+  //     }
+  //   }
+  // });
+  data.map = _.map(data.map, a => ({x: a[0], y: a[1]}));
+  data.guards = _.map(data.guards, a => ({x: a[0], y: a[1]}));
+  let result = alg(data.map, data.guards);
+  if(result !== null) {
+    console.log('1: (' + result.x + ', ' + result.y + ')');
+  } else {
+    console.log("1: The entire area is covered");
+  }
 }
 
 main();
